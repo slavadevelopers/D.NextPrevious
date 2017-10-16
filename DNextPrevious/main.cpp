16a51c831c997e61eba4e3ad4198dc00df924ee4
Vyacheslav Mukhin <slavadevelopers@gmail.com>
2017-10-16 21:57:46 +0300

    fixed bug

--- DNextPrevious/main.cpp
+++ DNextPrevious/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
     short int N;
     cin >> N;
-    cout << "The next number for the number " << N << " is " << N + 1 << endl;
-    cout << "The previous number for the number " << N << " is " << N - 1 <<endl;
+    cout << "The next number for the number " << N << " is " << N + 1 <<"!"<< endl;
+    cout << "The previous number for the number " << N << " is " << N - 1 <<"!"<<endl;
     return 0;
 }
