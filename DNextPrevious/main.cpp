607f70b84063f0476d77453780e35a0f8e99ad7e
Vyacheslav Mukhin <slavadevelopers@gmail.com>
2017-10-16 21:54:20 +0300

    Test D is completed

---
+++ DNextPrevious/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    short int N;
+    cin >> N;
+    cout << "The next number for the number " << N << " is " << N + 1 << endl;
+    cout << "The previous number for the number " << N << " is " << N - 1 <<endl;
+    return 0;
+}
